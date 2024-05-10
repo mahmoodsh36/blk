35fdd49650323f1dedcd6597b2862d7f9ce639aa
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-10 17:49:07 +0300

    use \hyperref instead of \ref in exports

--- blk-org.el
+++ blk-org.el
@@ -56,7 +56,7 @@ If LINK is not found, just return it as is."
         (cond
          ((eq format 'html) (format "<a href=\"%s.html\">%s</a>" linked-file-no-ext desc))
          ((eq format 'md) (format "[%s](%s.md)" desc linked-file-no-ext))
-         ((eq format 'latex) (format "\\ref{%s}" link))
+         ((eq format 'latex) (format "\\hyperref[%s]{%s}" link (or desc link)))
          (t link)))
     link))
 
